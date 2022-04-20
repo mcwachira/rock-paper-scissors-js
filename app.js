f19bc1c5f67cf6fe6f15d0c9f31422a6bad4a3b6
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-04-20 10:45:38 +0300

    enabled users choice to be displayed when image is clicked and enabled pc to pick a random image and displaed it

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
 const HandLayout = document.querySelector('.hands')
 
 const Contest = document.querySelector('.contest')
-const myChoice = document.querySelector('.my__choice')
-const pcChoice = document.querySelector('.pc__choice')
+
+const userPick = document.getElementById('userPickedImage');
+const housePick = document.getElementById('housePickedImage')
+
+const imagesData = [
+    {
+        id: 1,
+        name: 'paper',
+        src: "assets/Paper.png"
+    }
+    ,
+    {
+        id: 2,
+        name: 'scissors',
+        src: "assets/Scissors.png"
+    }
+    ,
+    {
+        id: 3,
+        name: 'rock',
+        src: "assets/Rock.png"
+    }
+
+]
+
 const pickUsersHand = (hand) => {
 
-    // HandLayout.st
-    //hide the hand layout
+
     HandLayout.classList.add('display')
-    // console.log(hand.parentElement.nodeName)
-    //display the game layout
-    // const image = document.createElement('img')
-    // image.textContent = hand.parentElement.nodeName;
-    // myChoice.appendChild(image)
+
+    console.log(hand)
+
+    imagesData.map((image) => {
+        if (image.name === hand) {
+            userPick.src = image.src
+        }
+
+
+    })
+    housePickedHand(imagesData)
     Contest.classList.add('active')
 
+}
+
+const housePickedHand = (imagesData) => {
+    console.log(imagesData.length);
+    const randomNumber = Math.floor(Math.random() * imagesData.length + 1)
+    console.log(randomNumber)
+
+    imagesData.map((image) => {
+        if (randomNumber === image.id) {
+            housePick.src = image.src
+        }
+    })
 }
